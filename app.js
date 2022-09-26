4fe13bf8af88830bf3fae32eab5397735046461f
Nathaniel Boyd <nathanielboydvillarroel@gmail.com>
2022-09-26 00:53:36 -0300

    mercado de conejos v1.0

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, ('/public'))));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
+
+app.get('/ingreso', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/ingreso.html'))
+});
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/registro.html'))
+});
+
+
+app.listen(3000, function () {
+    console.log('Servidor corriendo en puerto 3000')
+})
+
